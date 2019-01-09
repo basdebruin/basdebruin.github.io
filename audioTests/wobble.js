227bd42152afb9e1256eefcd93585f0a94868804
Bas <bruin998@gmail.com>
2019-01-09 15:12:29 +0100

    wobble fix

--- audioTests/wobble.js
+++ audioTests/wobble.js
@@ -19,7 +19,19 @@ function setup() {
 	osc2.start();
 }
 
-function draw() {
+function draw() {	
+	if (mouseIsPressed) {
+		osc1.freq(100 + rotationX * 5);
+		osc2.freq(random(99, 102) + rotationX * 5);
+		osc1.width(constrain(rotationY / 180 - 90, 0, 1));
+		r = constrain(rotationX * 4,0,255);
+		b = constrain(rotationY * 2 + 180,0,255);
+		g = 240;
+		txtStr = "W O B B L E";
+	}
+	else {
+		txtStr = "T O U C H"
+	}
 	background(r,g,b);
 	fill(255 - r, 255 - g, 255 - b);
 	textSize(28);
@@ -31,23 +43,16 @@ function mousePressed() {
 	//Nodig voor audio
 	if (getAudioContext().state !== 'running') {
 		getAudioContext().resume();
-	}
-	osc1.amp(0.5, 0.5);
-	osc2.amp(0.5, 0.5);
-	osc1.freq(100 + rotationX * 5);
-	osc2.freq(97 + rotationX * 5);
-	osc1.width(constrain(rotationY / 180 - 90, 0, 1));
-	r = constrain(rotationX * 4,0,255);
-	b = constrain(rotationY * 2 + 180,0,255);
-	g = 240;
-	txtStr = "W O B B L E";
+	} 
+	osc1.amp(0.5, 0.1);
+	osc2.amp(0.5, 0.1);
+	
 }
 function mouseReleased() {
-	osc1.amp(0, 1);
-	osc2.amp(1, 1);
+	osc1.amp(0.001, 1);
+	osc2.amp(0.001, 1);
 	rotate(0);
 	r = 40;
 	g = 40;
 	b = 40;
-	txtStr = "T O U C H"
 }
