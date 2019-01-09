2d7a199014ace84bd7b2240bc0614fbe37eb6a14
Bas <bruin998@gmail.com>
2019-01-09 14:57:00 +0100

    wobble audio test

---
+++ audioTests/wobble.js
@@ -0,0 +1,53 @@
+var osc1;
+var osc2;
+var r = 40;
+var g = 40;
+var b = 40;
+var txtStr = "T O U C H";
+
+function setup() {
+	createCanvas(windowWidth,windowHeight);
+	background(r,g,b);
+	//AUDIO
+	osc1 = new p5.Pulse();
+	osc2 = new p5.Pulse();
+	osc1.freq(100);
+	osc2.freq(100);
+	osc1.amp(0);
+	osc2.amp(0);
+	osc1.start();
+	osc2.start();
+}
+
+function draw() {
+	background(r,g,b);
+	fill(255 - r, 255 - g, 255 - b);
+	textSize(28);
+	text(txtStr, width/2, height/2);
+	textAlign("center");
+}
+
+function mousePressed() {
+	//Nodig voor audio
+	if (getAudioContext().state !== 'running') {
+		getAudioContext().resume();
+	}
+	osc1.amp(0.5, 0.5);
+	osc2.amp(0.5, 0.5);
+	osc1.freq(100 + rotationX * 5);
+	osc2.freq(97 + rotationX * 5);
+	osc1.width(constrain(rotationY / 180 - 90, 0, 1));
+	r = constrain(rotationX * 4,0,255);
+	b = constrain(rotationY * 2 + 180,0,255);
+	g = 240;
+	txtStr = "W O B B L E";
+}
+function mouseReleased() {
+	osc1.amp(0, 1);
+	osc2.amp(1, 1);
+	rotate(0);
+	r = 40;
+	g = 40;
+	b = 40;
+	txtStr = "T O U C H"
+}
