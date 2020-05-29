0a1b54398820257c76dae29e2200339b68bf70cc
Bas de Bruin <bruin998@gmail.com>
2020-05-29 15:53:42 +0200

    update to gppac microsite, added map

--- demo/gppac/assets/js/head.js
+++ demo/gppac/assets/js/head.js
@@ -5,20 +5,22 @@ var header, menuButton;
 var activeClass = 'active';
 
 function init() {
-    console.log('init');
-
+    // init AOS
     AOS.init({
         duration: 800,
         ease: 'ease-in-out'
     });
 
-    // get header
+    // get HEADER
     header = document.getElementById('header');
     // get menuButton
     menuButton = document.getElementById('menu-button');
     
     // on button click
     menuButton.addEventListener('click', toggleMenu);
+
+    // ACCORDION
+    initAccordion();
 }
 
 function toggleMenu() {
@@ -26,5 +28,24 @@ function toggleMenu() {
         header.classList.remove(activeClass)
     } else {
         header.classList.add(activeClass);
+    }
+}
+
+
+
+var accordions;
+// ACCORDION
+function initAccordion() {
+    // get
+    accordions = document.getElementsByClassName('accordion');
+
+    // add eventlisteners to all buttons
+    for (accordion of accordions) {
+        let button = accordion.querySelector('button');
+        button.addEventListener('click', event => {
+            let acc = event.target.parentElement;
+            if (acc.classList.contains(activeClass)) acc.classList.remove(activeClass);
+            else acc.classList.add(activeClass);
+        })
     }
 }
