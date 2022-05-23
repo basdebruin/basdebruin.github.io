ec18eed78763f9cf595d26c0377d1a63a73087e9
Bas de Bruin <bruin998@gmail.com>
2022-05-23 23:45:12 +0200

    add animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import Swup from 'swup';
 import SwupScrollPlugin from '@swup/scroll-plugin';
 import { setupSounds, attachSounds } from "./sounds";
+import setupAnimationElements from './scrollAnimation';
 
 // trigger init()
 if (document.readyState === "complete" || document.readyState === "interactive")
@@ -21,75 +22,12 @@ function init() {
         swup.scrollTo(0)
     });
 
-    // trigger scroll function
-    setupScroll();
-    swup.on('contentReplaced', setupScroll);
+    // handle scroll animations
+    setupAnimationElements();
 
     // create sounds
     setupSounds();
     // attach sounds
     attachSounds();
     swup.on('contentReplaced', attachSounds);
-}
-
-function setupScroll() {
-
-    if(window.location.pathname == '/') {
-        window.onscroll = handleScroll();
-    } else {
-        window.onscroll = null;
-    }
-
-}
-
-/**
- * Scroll handler, returns function to be called on home page scroll
- * @returns {()=>void}
- */
-function handleScroll() {
-    const headerElem = document.getElementsByTagName('header')[0];
-    const indicatorElem = headerElem.querySelector('.indicator');
-
-    const workHeight = document.getElementById('work').getBoundingClientRect().top;
-    const contactHeight = document.getElementById('contact').getBoundingClientRect().top;
-
-    return function() {
-        handleHeaderPosition(headerElem);
-        handleHeaderIndicator(indicatorElem, workHeight, contactHeight);
-    }
-}
-
-/**
- * 
- * @param {Element} headerElem 
- * @returns {()=>void}
- */
-function handleHeaderPosition(headerElem) {
-    if (window.scrollY < 100) {
-        headerElem.classList.add('lowered');
-        return;
-    }
-    headerElem.classList.remove('lowered');
-}
-
-/**
- * 
- * @param {Element} indicatorElem 
- * @param {number} workHeight 
- * @param {number} contactHeight 
- * @returns {()=>void}
- */
-function handleHeaderIndicator(indicatorElem, workHeight, contactHeight) {
-    if (window.scrollY + 50 > contactHeight) {
-        indicatorElem.classList.add('scrolled-to-contact');
-        return;
-    }
-    if (window.scrollY + 50 > workHeight) {
-        indicatorElem.classList.remove('scrolled-to-contact');
-        indicatorElem.classList.add('scrolled-to-work');
-        return;
-    }
-
-    indicatorElem.classList.remove('scrolled-to-contact');
-    indicatorElem.classList.remove('scrolled-to-work');
 }
--- scripts/scrollAnimation.ts
+++ scripts/scrollAnimation.ts
@@ -2,6 +2,25 @@
  * Scroll animations using IntersectionObserver
  */
 
-export function setupAnimationElements() {
+export default function setupAnimationElements(className : string = 'scroller') : void {
+
+    const elements = document.getElementsByClassName(className);
+
+    const observer = new IntersectionObserver(checkElements, {
+        threshold: 0.3
+    })
+
+    function checkElements(entries, observer) : IntersectionObserverCallback {
+        entries.forEach(entry => {
+            entry.target.classList.toggle('scroller-in', entry.isIntersecting)
+        });
+        return;
+    }
+
+    setTimeout(() => {
+        for (let elem of elements) {
+            observer.observe(elem);
+        }
+    }, 100);
     
 }
